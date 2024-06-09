afe43bf1649d4ffb5cd5b32b461af132ad7e1f65
Sarvesh Palav <sarveshpalav@gmail.com>
2024-06-09 13:59:36 +0530

    feat: fixed grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,36 @@ export default function Home() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
         <div className=" p-6 rounded-lg">
            
-        <h1 className="header font-inter text-2xl sm:text-3xl md:text-4xl  xl:text-5xl">Sarvesh Palav</h1>
-        <h2 className="sub-heading font-inter text-base sm:text-lg mt-4 md:ml-2">Full Stack Application Developer</h2>
+        <div className="text-center sm:text-left"> 
+    <h1 className="header font-inter text-2xl sm:text-3xl md:text-4xl xl:text-5xl">Sarvesh Palav</h1>
+    <h2 className="sub-heading font-inter text-base sm:text-lg mt-4 md:ml-2">Full Stack Application Developer</h2>
+</div>
 
         </div>
-        <div className="font-inter p-6  brief">
+        <div className="font-inter p-6  brief text-center sm:text-left">
         Over the years, I’ve built products for companies and businesses around the globe, ranging from marketing websites to complex solutions and enterprise apps, with a focus on fast, elegant, and accessible user experiences.
 Currently, I work at Optel Group as a Software Consultant, crafting thoughtful and inclusive experiences that adhere to web standards for diverse clients across the traceability domain. I leverage technologies like Python, FastAPI, PostgreSQL, React-TypeScript, and Google Cloud Platform to deliver scalable and reliable solutions.
 
         </div>
         
       
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full mt-2 md:col-span-3 ">
-        <div className=" mx-4 md:mx-0">
-        <button type="button" className="text-white hover:text-white border border-white-700 hover:bg-white-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-white-500 dark:text-white dark:hover:text-white dark:hover:bg-white-500 dark:focus:ring-white-800">Contact Me</button>
-        <button type="button" className="text-white hover:text-white border border-white-700 hover:bg-white-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-white-500 dark:text-white dark:hover:text-white dark:hover:bg-white-500 dark:focus:ring-white-800">Download Resume</button>
-
+        <div className="grid grid-cols-1 md:grid-cols-8 justify-center gap-4 w-full mt-2 md:col-span-3">
+    <div style={{justifySelf:"center"}}className="mx-5 md:mx-6  col-span-2 flex  ">
+        <div >
+            <button style={{width: "10rem"}} type="button" className="text-white hover:text-white border border-white-700 hover:bg-white-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-white-500 dark:text-white dark:hover:text-white dark:hover:bg-white-500 dark:focus:ring-white-800">Contact Me</button>
         </div>
-        <div className="font-inter p-6 rounded-lg ">
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full px-4">
+       
+    </div>
+    <div style={{justifySelf:"center"}}className="mx-5 md:mx-6  col-span-2 flex  ">
+        <div >
+            <button style={{width: "10rem"}} type="button" className="text-white hover:text-white border border-white-700 hover:bg-white-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-white-500 dark:text-white dark:hover:text-white dark:hover:bg-white-500 dark:focus:ring-white-800">Download Resume</button>
+        </div>
+       
+    </div>
+  
+    <div style={{justifySelf:"center"}} className="mx-4 my-4 md:mx-6  md:col-span-4 flex ">
+       
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full px-4">
   <div>
     <span className="bg-gray-100 text-gray-800 text-sm font-medium px-4 py-1 rounded dark:bg-gray-700 dark:text-gray-300">HTML</span>
   </div>
@@ -62,9 +73,11 @@ Currently, I work at Optel Group as a Software Consultant, crafting thoughtful a
   </div>
 </div>
 
-       
-        </div>
-        </div>
+   </div>
+    
+</div>
+        
+        
         </div>
        
     </main>
