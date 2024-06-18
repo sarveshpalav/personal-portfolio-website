163a3196b731c3fe948538abed7262195d6d90fc
Sarvesh Palav <sarveshpalav@gmail.com>
2024-06-18 19:49:58 +0530

    feat: adjust responsive padding for root container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function App() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-6 md:pt-20 md:pl-32 md:pr-32 lg:pl-48 lg:pr-48 xl:pl-64 xl:pr-64">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-6 md:pt-20 md:pl-32 md:pr-32 lg:pl-48 lg:pr-48 xl:pl-48 xl:pr-48">
     <HomePage></HomePage>
 </main>
   );
